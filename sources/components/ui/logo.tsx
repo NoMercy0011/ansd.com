6cb33935efd6987b43dbeec4164c3d0b8b907754
NoMercy0011 <luc.roberto.randria@gmail.com>
2025-12-08 14:19:47 +0300

    fix: update logo import to use relative path

--- sources/components/ui/logo.tsx
+++ sources/components/ui/logo.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
-import logo from '@/public/logo.png'
 
 export default function Logo() {
   return (
     <div className="w-15 h-15 bg-red-500 rounded-2xl flex items-center justify-center shadow-lg border">
-        <Image src={logo} alt='logo ans.com' className='rounded-2xl shadow shadow-card bg-red-500'/>
+        <Image src="logo.png" alt='logo ans.com' className='rounded-2xl shadow shadow-card bg-red-500'/>
     </div>
   )
 }
