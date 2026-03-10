9d55f26efc5bdf03e793550712a1d37a12dbf40e
NoMercy0011 <luc.roberto.randria@gmail.com>
2026-03-10 21:33:24 +0300

    feat: update next.config.ts to include output, basePath, and assetPrefix configurations

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: process.env.BASE_PATH ? process.env.BASE_PATH : "",
+  assetPrefix: process.env.URL ? process.env.URL : undefined,
   /*images: {
     unoptimized: true,
   },*/
